5af321d9ebd3cb08637d31f3a8af2d0cdbf97a52
nico-arnold <82146516+nico-arnold@users.noreply.github.com>
2024-09-10 07:30:36 +0200

    Change BLE name for better recogition

--- ATCwatch/ble.cpp
+++ ATCwatch/ble.cpp
@@ -24,10 +24,10 @@ BLECharacteristic   RXchar        = BLECharacteristic("0001", BLEWriteWithoutRes
 bool vars_ble_connected = false;
 
 void init_ble() {
-  blePeripheral.setLocalName("ATCwatch");
+  blePeripheral.setLocalName("ATCwatch-M");
   blePeripheral.setConnectionInterval(400,401);
   blePeripheral.setAdvertisingInterval(500);
-  blePeripheral.setDeviceName("ATCwatch");
+  blePeripheral.setDeviceName("ATCwatch-M");
   blePeripheral.setAdvertisedServiceUuid(main_service.uuid());
   blePeripheral.addAttribute(main_service);
   blePeripheral.addAttribute(TXchar);
